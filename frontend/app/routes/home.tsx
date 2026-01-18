25509c51291882cd544487d8f832de5b9d2c0aa6
ramsy0dev <ramsy0dev@gmail.com>
2026-01-18 01:36:41 +0100

    Update the frontend react template

--- frontend/app/routes/home.tsx
+++ frontend/app/routes/home.tsx
@@ -3,8 +3,8 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "webapp-template home page" },
+    { name: "description", content: "Welcome to webapp-template Home page!" },
   ];
 }
 
